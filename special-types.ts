bcc25bd3e7e22a67681eeca080799c31416d0794
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-19 17:00:35 +0200

    feat: DOM element type

---
+++ special-types.ts
@@ -0,0 +1,13 @@
+// access to DOM element
+
+const inputEl = document.getElementById("user") as HTMLInputElement;
+
+console.log(inputEl?.value);
+
+/*
+const inputIn = document.getElementById("user")!
+
+console.log(inputIn.value);
+
+*
+*/
